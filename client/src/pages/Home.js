f9119813583ea2b8a2f7640b634ba5d511e88322
vaibhav519 <vaibhav3raj@gmail.com>
2023-06-21 00:59:36 +0530

    Initial Commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,6 +14,49 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.ok) {
+          const result = await response.json();
+          console.log("here")
+          setAllPosts(result.data.reverse);
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPosts.filter(
+          (item) =>
+            item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+            item.prompt.toLowerCase().includes(searchText.toLowerCase())
+        );
+
+        setSearchResults(searchResults);
+      }, 500)
+    );
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -27,7 +70,14 @@ const Home = () => {
         </p>
       </div>
       <div className="mt-16">
-        <FormField />
+        <FormField
+          labelName="Search posts"
+          type="text"
+          name="text"
+          placeholder="Search posts"
+          value={searchText}
+          handleChange={handleSearchChange}
+        />
       </div>
 
       <div className="mt-10">
@@ -41,16 +91,14 @@ const Home = () => {
               <h2 className="font-medium text-[#666e75] text-xl mb-3">
                 Showing results for{" "}
                 <span className="text-[#222328]">{searchText}</span>
+                <span className="text-[#222328]">{searchText}</span>
               </h2>
             )}
             <div className="grid lg:grid-cols-4 sm:grid-col-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards
-                  data={[]}
-                  title="No search results found"
-                />
+                <RenderCards data={searchResults} title="No search results found" />
               ) : (
-                <RenderCards data={[]} title="No  posts found" />
+                <RenderCards data={allPosts} title="No  posts found" />
               )}
             </div>
           </>
